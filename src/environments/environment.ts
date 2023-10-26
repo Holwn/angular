ca613e8ea57e1aee92154f188956865825d2aaa1
Holwn <ltienhoang2@gmail.com>
2023-10-26 18:54:47 +0700

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'HolwnWeb',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44375/',
+    redirectUri: baseUrl,
+    clientId: 'HolwnWeb_App',
+    responseType: 'code',
+    scope: 'offline_access HolwnWeb',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44316',
+      rootNamespace: 'HolwnWeb',
+    },
+  },
+} as Environment;
